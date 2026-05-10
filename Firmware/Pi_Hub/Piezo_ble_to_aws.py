d6cd5646d23bf61c046011cd01353d2563194e69
Elijah <eppselijah77@gmail.com>
2026-05-10 12:30:19 -0400

    Update AWS IoT host address placeholder

--- Firmware/Pi_Hub/Piezo_ble_to_aws.py
+++ Firmware/Pi_Hub/Piezo_ble_to_aws.py
@@ -14,7 +14,7 @@ TX_UUID     = "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"  # ESP32 notifies on this
 # ─────────────────────────────────────────────
 # AWS IoT CONFIG
 # ─────────────────────────────────────────────
-HOST      = "a3lz5fjtuoifub-ats.iot.us-east-2.amazonaws.com"
+HOST      = "ABC123Placeholder.iot.us-east-2.amazonaws.com"
 CERT_PATH = "cert/"
 CLIENT_ID = "Pi_400_BLE_Bridge"
 TOPIC     = "ESP32/FootStep"          
